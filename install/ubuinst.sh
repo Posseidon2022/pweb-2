931f3df09f69c048b7092dc22af4b86c9fe906fc
Agostinho Carrara <108548804+SrCarrara@users.noreply.github.com>
2022-07-17 11:17:46 -0300

    Add files via upload

--- install/ubuinst.sh
+++ install/ubuinst.sh
@@ -131,7 +131,6 @@ clear
 msg -bar
 echo -e "\033[1;31mPAINEL JÁ INSTALDO EM SUA VPS, RECOMENDO\033[0m"
 echo -e "\033[1;31mUMA FORMATAÇÃO PARA UMA NOVA INSTALÇÃO!\033[0m"
-msg -bar
 sleep 5
 systemctl restart apache2 > /dev/null 2>&1
 cat /dev/null > ~/.bash_history && history -c
@@ -140,7 +139,6 @@ exit;
 else
   echo -e 'by: @PainelWebAI' >/usr/lib/telegram
   echo -e "\e[1;97m           \e[5m\033[1;100m   INSTALADOR PAINEL WEB A.I ⚡   \033[1;37m"
- echo ""
  echo ""
  echo -e "\033[1;37m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\033[0m"
 echo -e "\033[1;37m┃[ ! ] ESTA INSTALAÇÃO FORNECE UM CONJUNTO DE FERRAMENTAS PARA\033[38;5;197m\033[38;5;197m\033[1;37m ┃\E[0m"
